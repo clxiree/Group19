f4f3524dabd625ccdfddd5a6aa356a53f9e7395a
choowieeee <christinchoo03@gmail.com>
2024-11-14 11:00:34 +0800

    converted headers for tutees

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,8 +29,8 @@
 
   function mobileNavToogle() {
     document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
+    // mobileNavToggleBtn.classList.toggle('bi-list');
+    // mobileNavToggleBtn.classList.toggle('bi-x');
   }
   mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
 
